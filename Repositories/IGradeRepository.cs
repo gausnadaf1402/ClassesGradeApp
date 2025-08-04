bb9eccfba8ad1c2189f9b953543a4346f9edebc3
Gaus <nadafgaus1402@gmail.com>
2025-08-04 14:04:55 +0530

    Add project files.

---
+++ Repositories/IGradeRepository.cs
@@ -0,0 +1,12 @@
+﻿using ClassesGradeApp.Models;
+using System.Data;
+
+namespace ClassesGradeApp.Repositories
+{
+    public interface IGradeRepository
+    {
+        void AddGrade(Grade grade);
+        void UpdateGrade(Grade grade);
+        void DeleteGrade(int gradeId);
+    }
+}
